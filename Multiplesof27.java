40a20f0f00c39c8763ad822093b909c0f5ba58bd
Akshai-1513 <akshaikumartn@gmail.com>
2025-01-29 22:48:32 +0530

    Multipiles of 27

---
+++ Multiplesof27.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplesof27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        if (num % 27 == 0)
+            System.out.println(num + " is a multiple of 27");
+        else
+            System.out.println(num + " is not a multiple of 27");
+        sc.close();
+    }
+}
