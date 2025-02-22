711b76671b9629b55fbfc0d7a5b06ca9c5aaa368
Akshai-1513 <akshaikumartn@gmail.com>
2025-02-22 23:26:12 +0530

    The add and sub of two random number  program is updated

--- Two_sum.java
+++ Two_sum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int remaining = target - nums[i];
@@ -11,11 +11,5 @@ class Solution {
             map.put(nums[i], i);
         }
         return new int[0];
-    }
-
-    public static void main(String[] args) {
-        int target = 9;
-        int[] nums = { 2, 7, 11, 15 };
-        twoSum(nums, target);
     }
 }
