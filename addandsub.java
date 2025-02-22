3400250372fbd6ef19306efb51f9e01db3382320
Akshai-1513 <akshaikumartn@gmail.com>
2025-02-22 23:24:55 +0530

    The add and sub program is updated

--- addandsub.java
+++ addandsub.java
@@ -2,20 +2,25 @@ import java.util.Random;
 
 public class addandsub {
     public static void main(String[] args) {
-        Random rad = new Random();
-        int num1 = rad.nextInt(100);
-        System.out.println("The First integer is " + num1);
-        int num2 = rad.nextInt(100);
-        System.out.println("The second integer is " + num2);
-        int num3;
+        // Random rad = new Random();
+        // int num1 = rad.nextInt(100);
+        // System.out.println("The First integer is " + num1);
+        // int num2 = rad.nextInt(100);
+        // System.out.println("The second integer is " + num2);
+        // int num3;
 
-        System.out.println("The addition of the two number is " + (num1 + num2));
+        // System.out.println("The addition of the two number is " + (num1 + num2));
 
-        num3 = num1 - num2;
-        if (num3 < 0) {
-            num3 = num3 * -1; // If the substracted value is negative ,it will change it to positive
-        }
-        System.out.println("The substraction of the two number is " + num3);
+        // num3 = num1 - num2;
+        // if (num3 < 0) {
+        // num3 = num3 * -1; // If the substracted value is negative ,it will change it
+        // to positive
+        // }
+        // System.out.println("The substraction of the two number is " + num3);
+
+        int num = 10;
+        System.out.println(((Object) num).getClass().getSimpleName()); // Output: Integer
+
     }
 
 }
