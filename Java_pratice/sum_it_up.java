1e7ad9437bad59af4f2c57f3403bf622ba9679be
Akshai-1513 <akshaikumartn@gmail.com>
2025-03-09 00:14:08 +0530

    The program is to fin the sum of the number till it get single digit.

--- Java_pratice/sum_it_up.java
+++ Java_pratice/sum_it_up.java
@@ -5,10 +5,12 @@ public class sum_it_up {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int sum = 0;
-        while(num != 0){
+        while (num != 0) { // The loop continues till the num value becomes zero
             sum += (num % 10);
             num /= 10;
-            if(num == 0 && sum > 9){
+
+            // This line reduces the time complexity
+            if (num == 0 && sum > 9) {
                 num = sum;
                 sum = 0;
             }
