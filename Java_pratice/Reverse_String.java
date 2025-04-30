f523fee5f63974782ff008241e94661d8eb7bad4
Akshai-1513 <akshaikumartn@gmail.com>
2025-04-30 18:39:09 +0530

    Updated program file

--- Java_pratice/Reverse_String.java
+++ Java_pratice/Reverse_String.java
@@ -7,13 +7,15 @@ public class Reverse_String {
         System.out.print(" Enter the string : ");
         String str = sc.nextLine();
         System.out.println("The entered string is \"" + str + "\"");
-
+        System.out.println("The reversed string is : " + reverse_str(str));
+        sc.close();
+    }
+    public static String reverse_str(String str){
         String reverse = "";
         for (int i = 0; i < str.length(); i++) {
             char rev = str.charAt(i);
             reverse = rev + reverse;
         }
-        System.out.println("The reversed string is \"" + reverse + "\"");
-        sc.close();
+        return reverse;
     }
 }
