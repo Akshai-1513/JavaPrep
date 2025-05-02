f26f1ce7b50cafd92f1ab82ced126e300c0229eb
Akshai-1513 <akshaikumartn@gmail.com>
2025-05-02 23:31:11 +0530

    The updated unique element file

--- Java_pratice/Unique_element.java
+++ Java_pratice/Unique_element.java
@@ -2,26 +2,36 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
 public class Unique_element {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number : ");
         int num = sc.nextInt();
-        String str_num = Integer.toString(num);
-        char arr[] = str_num.toCharArray();
+        String num_string = num + "";
+        int[] arr = new int[num_string.length()];
+        for(int i = 0; i<num_string.length();i++){
+            arr[i] = num_string.charAt(i) - '0';
+        }
         System.out.println(Arrays.toString(arr));
 
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char i : arr){
-            map.put(i, map.getOrDefault(i,0)+1);
-        }
-        System.out.println(map);
+        if(unique_value(arr) != -1)
+            System.out.println("The unique element in the array is : " + unique_value(arr));
 
-        for (int i = arr.length-1; i >=0; i--) {
-            if(map.get(arr[i])==1) {
-                System.out.println(map.get(arr[i]));
-                return;
-            }
+        else
+            System.out.println("There is no unique element in the array.");
+
+    }
+
+    public static int unique_value(int arr[]){
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for(int num : arr){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        System.out.println(-1);
+        for (int num : arr){
+            if (map.get(num) == 1)
+                return num;
+        }
+        return -1;
     }
 }
